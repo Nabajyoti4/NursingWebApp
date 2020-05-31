eb661e37c18a5b28d6b0f6ad21fd20339bc28cca
Raju Moni Borah <raju42fun@gmail.com>
2020-05-31 22:49:47 +0530

    human readable timestamp

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -32,7 +32,7 @@
                             <td>{{$user->name}}</td>
                             <td>{{$user->email}}</td>
                             <td>{{$user->phone_no}}</td>
-                            <td>{{$user->created_at}}</td>
+                            <td>{{$user->created_at->diffForHumans()}}</td>
                         </tr>
                     @empty
                         <td>No users found</td>
