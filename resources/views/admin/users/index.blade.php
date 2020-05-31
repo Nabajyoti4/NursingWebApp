146bbffd2b5def9a6f09e868f9e0f0e15bf42cbe
Raju Moni Borah <raju42fun@gmail.com>
2020-05-31 22:37:44 +0530

    admin->user page added

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,9 +1,45 @@
-<h1>Users Admin panel</h1>
-<ul>
-@forelse($users as $user)
+@extends('layouts.admin')
+@section('title')
+    Users
+@endsection
 
-        <li>{{$user->name}}</li>
-@empty
-            <li>No users found</li>
-@endforelse
-</ul>
+@section('content')
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-2 text-gray-800">List of Admins</h1>
+
+    <!-- DataTales Example -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Users</h6>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                    <tr>
+                        <th>User ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone No</th>
+                        <th>Created at</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @forelse($users as $user)
+                        <tr>
+                            <td>{{$user->id}}</td>
+                            <td>{{$user->name}}</td>
+                            <td>{{$user->email}}</td>
+                            <td>{{$user->phone_no}}</td>
+                            <td>{{$user->created_at}}</td>
+                        </tr>
+                    @empty
+                        <td>No users found</td>
+                    @endforelse
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
