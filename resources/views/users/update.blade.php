68f179ffedaa70774caea3105a00bae24172f282
altafhussain <hussainaltafali35@gmail.com>
2020-06-01 21:17:15 +0530

    update

---
+++ resources/views/users/update.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Update</title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  <style type="text/css">
+  	.place{
+		margin-top: 10px;
+		border: 3px solid black;
+	}
+  </style>
+
+</head>
+<body>	
+	<div class="container col-lg-6 col-sm-10 place bg-light">
+  <form>
+    <div class="form-group font-weight-bold">
+      <label for="name">Full Name:</label>
+      <input type="text" class="form-control" id="pwd" placeholder="Enter Name">
+    </div>
+    <div class="form-group font-weight-bold">
+      <label for="name">Phone Number:</label>
+      <input type="number" class="form-control" id="pwd" placeholder="Enter Phone number">
+    </div>
+    <div class="text-center">
+    	<label class="form-group font-weight-bold">Current Address</label>
+    </div>
+    <div class="row">
+    	<div class="col-lg-4 p-2">
+    	<input type="text" class="form-control text-center" id="pwd" placeholder="Street name">	
+    	</div>
+    	<div class="col-lg-4 p-2">
+    	<input type="text" class="form-control text-center" id="pwd" placeholder="Landmark">	
+    	</div>
+    	<div class="col-lg-4 p-2">
+    	<input type="text" class="form-control text-center" id="pwd" placeholder="District">	
+    	</div>
+    	<div class="col-lg-4 p-2">
+    	<input type="text" class="form-control text-center" id="pwd" placeholder="State">	
+    	</div>
+    	<div class="col-lg-4 p-2">
+    	<input type="text" class="form-control text-center" id="pwd" placeholder="Country">	
+    	</div>
+    	<div class="col-lg-4 p-2">
+    	<input type="text" class="form-control text-center" id="pwd" placeholder="Pin Code">	
+    	</div>
+    </div>
+    <div class="text-center">
+    	<label class="form-group font-weight-bold">Permanent Address</label>
+    </div>
+    <div class="row">
+    	<div class="col-lg-4 p-2">
+    	<input type="text" class="form-control text-center" id="pwd" placeholder="Street name">	
+    	</div>
+    	<div class="col-lg-4 p-2">
+    	<input type="text" class="form-control text-center" id="pwd" placeholder="Landmark">	
+    	</div>
+    	<div class="col-lg-4 p-2">
+    	<input type="text" class="form-control text-center" id="pwd" placeholder="District">	
+    	</div>
+    	<div class="col-lg-4 p-2">
+    	<input type="text" class="form-control text-center" id="pwd" placeholder="State">	
+    	</div>
+    	<div class="col-lg-4 p-2">
+    	<input type="text" class="form-control text-center" id="pwd" placeholder="Country">	
+    	</div>
+    	<div class="col-lg-4 p-2">
+    	<input type="text" class="form-control text-center" id="pwd" placeholder="Pin Code">	
+    	</div>
+    </div>
+    <div class="form-group text-center font-weight-bold">
+    <button type="submit" class="btn btn-primary">Submit</button>
+	</div>
+
+
+  </form>
+</body>
+</html>
