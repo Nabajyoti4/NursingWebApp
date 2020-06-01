f3b37f47e164270cb02a5dfcee33f36608dab481
Nabajyoti <sirnaba@gmail.com>
2020-06-01 14:26:39 +0530

    Working on User profile section

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,1 +1,53 @@
-<?php
+@extends('layouts.home')
+@section('title')
+    Edit User
+@endsection
+
+@section('content')
+
+    <div class="container-fluid profile-bg p-3">
+        <!-- navbar start -->
+    @include('partials.navbar')
+    <!-- navbar ends -->
+        <div class="container emp-profile mt-3">
+    <form class="">
+        @csrf
+
+        <div class="form-row">
+            <div class="col-3 mb-3">
+                <label for="name">FullName</label>
+                <input type="text" class="form-control" id="name" name="name" value="{{$user->name}}" required>
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="col-3 mb-3">
+                <label for="phone_no">Phone No</label>
+                <input type="text" class="form-control" id="phone_no" name="phone_no" value="{{$user->phone_no}}"
+                       required>
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="col-3 mb-3">
+                <label for="address1">Address 1</label>
+                <textarea class="form-control" id="address1" name="address1" required rows="4"
+                          placeholder="Eg:Room No
+Street name, landmark
+district
+state, country"></textarea>
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="col-3 mb-3">
+                <label for="address2">Address 2</label>
+                <textarea class="form-control" id="address2" name="address2" rows="4" placeholder="Eg:Room No
+Street name, landmark
+district
+state, country"
+                          required></textarea>
+            </div>
+        </div>
+        <button class="btn profile-edit-btn" type="submit">Update</button>
+    </form>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,154 @@
+@extends('layouts.home')
+
+@section('title')
+    User Profile
+@endsection
+
+@section('content')
+
+<div class="container-fluid profile-bg p-3">
+    <!-- navbar start -->
+@include('partials.navbar')
+<!-- navbar ends -->
+    <div class="container emp-profile mt-3">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="profile-img">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS52y5aInsxSm31CvHOFHWujqUx_wWTS9iM6s7BAm21oEN_RiGoog" alt=""/>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="profile-head">
+                        <h5>
+                            {{$user->name}}
+                        </h5>
+                        <h6>
+                            {{"Role"}}
+                        </h6>
+                        <ul class="nav nav-tabs pt-5" id="myTab" role="tablist">
+                            <li class="nav-item">
+                                <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">About</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Booking Details</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <a href="{{ route('users.edit', $user->id) }}">
+                        <div  class="profile-edit-btn text-center">Edit Profile</div>
+                    </a>
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 p-5">
+                    <div class="tab-content profile-tab" id="myTabContent">
+                        <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <label>User Id</label>
+                                </div>
+                                <div class="col-md-6">
+                                    <p>{{$user->id}}</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <label>Name</label>
+                                </div>
+                                <div class="col-md-6">
+                                    <p>{{$user->name}}</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <label>Email</label>
+                                </div>
+                                <div class="col-md-6">
+                                    <p>{{$user->email}}</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <label>Phone</label>
+                                </div>
+                                <div class="col-md-6">
+                                    <p>{{$user->phone_no}}</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <label>Curret Address</label>
+                                </div>
+                                <div class="col-md-6">
+                                    <p>Delhi</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <label>Permanent Address</label>
+                                </div>
+                                <div class="col-md-6">
+                                    <p>Delhi</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <label>Experience</label>
+                                </div>
+                                <div class="col-md-6">
+                                    <p>Expert</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <label>Hourly Rate</label>
+                                </div>
+                                <div class="col-md-6">
+                                    <p>10$/hr</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <label>Total Projects</label>
+                                </div>
+                                <div class="col-md-6">
+                                    <p>230</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <label>English Level</label>
+                                </div>
+                                <div class="col-md-6">
+                                    <p>Expert</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <label>Availability</label>
+                                </div>
+                                <div class="col-md-6">
+                                    <p>6 months</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <label>Your Bio</label><br/>
+                                    <p>Your detail description</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+</div>
+
+
+@endsection
