a63f4bd77af88ebe8e2a2c6a842c5285efe286e0
Raju Moni Borah <raju42fun@gmail.com>
2020-06-04 09:19:35 +0530

    Update admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -77,12 +77,12 @@
                 <span>Users</span></a>
         </li>
     <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseRequest"
            aria-expanded="true" aria-controls="collapseUtilities">
             <i class="fas fa-user"></i>
             <span>Requests</span>
         </a>
-        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
+        <div id="collapseRequest" class="collapse" aria-labelledby="headingUtilities"
              data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <h6 class="collapse-header">Requests:</h6>
