742f18a589f3baeb7613ae488092783712902346
Raju Moni Borah <raju42fun@gmail.com>
2020-07-12 10:17:25 +0530

    Update index.blade.php

--- resources/views/services/index.blade.php
+++ resources/views/services/index.blade.php
@@ -27,15 +27,11 @@
         <ul class="carousel-indicators">
             <li data-target="#demo" data-slide-to="0" class="active"></li>
             <li data-target="#demo" data-slide-to="1"></li>
-            <li data-target="#demo" data-slide-to="2"></li>
         </ul>
 
         <!-- The slideshow -->
         <div class="carousel-inner hero">
             <div class="carousel-item active">
-                <img src="{{asset('img/navbar-back-1.jpg')}}" alt="Los Angeles" >
-            </div>
-            <div class="carousel-item">
                 <img src="{{asset('img/slider-1.jpg')}}" alt="Chicago" >
             </div>
             <div class="carousel-item">
