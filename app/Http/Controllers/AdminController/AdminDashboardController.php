056e6270c202dfc903cbe0046b62e33e98f3853d
Raju Moni Borah <raju42fun@gmail.com>
2020-11-18 19:45:44 +0530

    Update AdminDashboardController.php

--- app/Http/Controllers/AdminController/AdminDashboardController.php
+++ app/Http/Controllers/AdminController/AdminDashboardController.php
@@ -109,7 +109,6 @@ class AdminDashboardController extends Controller
         // create  new attendence
         Attendance::create([
             'booking_id' => $booking->last()->id,
-            'photo' => 'null',
             'present' => 1,
             'nurse_id' => $id,
         ]);
